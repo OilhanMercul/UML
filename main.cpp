0e54a75ec2abcd83cbde321b05f40af32f2388cb
oilhan <oilhan.mercul@gmail.com>
2025-05-14 17:34:22 +0200

    clean

--- main.cpp
+++ main.cpp
@@ -33,66 +33,66 @@ using namespace std;
 
 //----------------------------------------------------- Méthodes publiques
 void test() {
-    std::cout << "\033[1;32mTest function called.\033[0m" << std::endl;
-    std::cout << "\033[1;34m---------------------\033[0m" << std::endl;
+    cout << "\033[1;32mTest function called.\033[0m" << endl;
+    cout << "\033[1;34m---------------------\033[0m" << endl;
 
-    std::cout << "\033[1;33mCreating a Provider object\033[0m" << std::endl;
+    cout << "\033[1;33mCreating a Provider object\033[0m" << endl;
     Provider provider(1, "password", "mail", "name");
-    std::cout << "\033[1;36mPrinting the provider details\033[0m" << std::endl;
+    cout << "\033[1;36mPrinting the provider details\033[0m" << endl;
     provider.print();
 
-    std::cout << "\033[1;33m---------------------\033[0m" << std::endl;
+    cout << "\033[1;33m---------------------\033[0m" << endl;
 
-    std::cout << "\033[1;33mCreating an AirCleaner object\033[0m" << std::endl;
+    cout << "\033[1;33mCreating an AirCleaner object\033[0m" << endl;
     AirCleaner airCleaner(1, "lat", "lon", Date(2023, 10, 1), Date(2023, 10, 31));
-    std::cout << "\033[1;36mPrinting the air cleaner details\033[0m" << std::endl;
+    cout << "\033[1;36mPrinting the air cleaner details\033[0m" << endl;
     airCleaner.print();
 
-    std::cout << "\033[1;33m---------------------\033[0m" << std::endl;
+    cout << "\033[1;33m---------------------\033[0m" << endl;
 
     provider.setAirCleaners({ airCleaner });
-    std::cout << "\033[1;36mPrinting the provider details again\033[0m" << std::endl;    
+    cout << "\033[1;36mPrinting the provider details again\033[0m" << endl;    
     provider.print();
 
-    std::cout << "\033[1;33m---------------------\033[0m" << std::endl;
+    cout << "\033[1;33m---------------------\033[0m" << endl;
 
-    std::cout << "\033[1;33mCreating a PrivateIndividual object\033[0m" << std::endl;
+    cout << "\033[1;33mCreating a PrivateIndividual object\033[0m" << endl;
     PrivateIndividual privateIndividual(1, "password", "mail", "firstName", "lastName", "phone", "address");
-    std::cout << "\033[1;36mPrinting the private individual details\033[0m" << std::endl;
+    cout << "\033[1;36mPrinting the private individual details\033[0m" << endl;
     privateIndividual.print();
 
-    std::cout << "\033[1;34m---------------------\033[0m" << std::endl;
+    cout << "\033[1;34m---------------------\033[0m" << endl;
 
-    std::cout << "\033[1;33mCreating a Sensor object\033[0m" << std::endl;
+    cout << "\033[1;33mCreating a Sensor object\033[0m" << endl;
     Sensor sensor(1, "lat", "lon");
-    std::cout << "\033[1;36mPrinting the sensor details\033[0m" << std::endl;
+    cout << "\033[1;36mPrinting the sensor details\033[0m" << endl;
     sensor.print();
-    std::cout << "\033[1;34m---------------------\033[0m" << std::endl;
+    cout << "\033[1;34m---------------------\033[0m" << endl;
 
-    std::cout << "\033[1;33mCreating a Measurement object\033[0m" << std::endl;
+    cout << "\033[1;33mCreating a Measurement object\033[0m" << endl;
     Measurement measurement(1, Date(2023, 10, 1), sensor, Attribut(1, "unit", "description"), 42.0);
-    std::cout << "\033[1;36mPrinting the measurement details\033[0m" << std::endl;
-    std::cout << "Measurement ID: " << measurement.getId() << std::endl;
-    std::cout << "Measurement Date: " << measurement.getDate().year << "-" << measurement.getDate().month << "-" << measurement.getDate().day << std::endl;
-    std::cout << "Measurement Sensor: " << measurement.getSensor().getId() << std::endl;
-    std::cout << "Measurement Attribut: " << measurement.getAttribut().getId() << std::endl;
-    std::cout << "Measurement Value: " << measurement.getValue() << std::endl;
-    std::cout << "\033[1;34m---------------------\033[0m" << std::endl;
+    cout << "\033[1;36mPrinting the measurement details\033[0m" << endl;
+    cout << "Measurement ID: " << measurement.getId() << endl;
+    cout << "Measurement Date: " << measurement.getDate().year << "-" << measurement.getDate().month << "-" << measurement.getDate().day << endl;
+    cout << "Measurement Sensor: " << measurement.getSensor().getId() << endl;
+    cout << "Measurement Attribut: " << measurement.getAttribut().getId() << endl;
+    cout << "Measurement Value: " << measurement.getValue() << endl;
+    cout << "\033[1;34m---------------------\033[0m" << endl;
 }
 
 void menuAgent(){
     int choix;
     do {
-        std::cout << "\nDear Agent, choose your action :" << std::endl;
-        std::cout << "1. Consult the quality of the air" << std::endl;
-        std::cout << "2. Compare a sensor to the other ones" << std::endl;
-        std::cout << "3. Consult statistics" << std::endl;
-        std::cout << "4. Consult the impact of air cleaners" << std::endl;
-        std::cout << "5. Analyse the data of a private individual" << std::endl;
-        std::cout << "6. Consult the list of the excluded users" << std::endl;
-        std::cout << "0. Go back" << std::endl;
-        std::cout << "Choice : ";
-        std::cin >> choix;
+        cout << "\nDear Agent, choose your action :" << endl;
+        cout << "1. Consult the quality of the air" << endl;
+        cout << "2. Compare a sensor to the other ones" << endl;
+        cout << "3. Consult statistics" << endl;
+        cout << "4. Consult the impact of air cleaners" << endl;
+        cout << "5. Analyse the data of a private individual" << endl;
+        cout << "6. Consult the list of the excluded users" << endl;
+        cout << "0. Go back" << endl;
+        cout << "Choice : ";
+        cin >> choix;
 
         switch (choix) {
             case 1: break;
@@ -102,7 +102,7 @@ void menuAgent(){
             case 5: break;
             case 6: break;
             case 0: return;
-            default: std::cout << "Invalid choice. Please choose again :.\n";
+            default: cout << "Invalid choice. Please choose again :.\n";
         }
     } while (choix != 0);
 }
@@ -110,14 +110,14 @@ void menuAgent(){
 void menuFournisseur(){
     int choix;
     do {
-        std::cout << "\nDear Provider, choose your action :" << std::endl;
-        std::cout << "1. Consult the quality of the air" << std::endl;
-        std::cout << "2. Compare a sensor to the other ones" << std::endl;
-        std::cout << "3. Consult statistics" << std::endl;
-        std::cout << "4. Consult the impact of air cleaners" << std::endl;
-        std::cout << "0. Go back" << std::endl;
-        std::cout << "Choice : ";
-        std::cin >> choix;
+        cout << "\nDear Provider, choose your action :" << endl;
+        cout << "1. Consult the quality of the air" << endl;
+        cout << "2. Compare a sensor to the other ones" << endl;
+        cout << "3. Consult statistics" << endl;
+        cout << "4. Consult the impact of air cleaners" << endl;
+        cout << "0. Go back" << endl;
+        cout << "Choice : ";
+        cin >> choix;
 
         switch (choix) {
             case 1: break;
@@ -125,7 +125,7 @@ void menuFournisseur(){
             case 3: break;
             case 4: break;
             case 0: return;
-            default: std::cout << "Invalid choice. Please choose again :.\n";
+            default: cout << "Invalid choice. Please choose again :.\n";
         }
     } while (choix != 0);
     return;
@@ -134,16 +134,16 @@ void menuFournisseur(){
 void menuUtilisateur(){
     int choix;
     do {
-        std::cout << "\nDear User, choose your action :" << std::endl;
-        std::cout << "1. Consult the quality of the air" << std::endl;
-        std::cout << "2. Compare a sensor to the other ones" << std::endl;
-        std::cout << "3. Consult statistics" << std::endl;
-        std::cout << "4. Contribute data" << std::endl;
-        std::cout << "5. Consult my points" << std::endl;
-        std::cout << "6. Consult the data of my own sensors" << std::endl;
-        std::cout << "0. Go back" << std::endl;
-        std::cout << "Choice : ";
-        std::cin >> choix;
+        cout << "\nDear User, choose your action :" << endl;
+        cout << "1. Consult the quality of the air" << endl;
+        cout << "2. Compare a sensor to the other ones" << endl;
+        cout << "3. Consult statistics" << endl;
+        cout << "4. Contribute data" << endl;
+        cout << "5. Consult my points" << endl;
+        cout << "6. Consult the data of my own sensors" << endl;
+        cout << "0. Go back" << endl;
+        cout << "Choice : ";
+        cin >> choix;
 
         switch (choix) {
             case 1: break;
@@ -153,7 +153,7 @@ void menuUtilisateur(){
             case 5: break;
             case 6: break;
             case 0: return;
-            default: std::cout << "Invalid choice. Please choose again :.\n";
+            default: cout << "Invalid choice. Please choose again :.\n";
         }
     } while (choix != 0);
 }
@@ -161,22 +161,22 @@ void menuUtilisateur(){
 void afficherMenuConnexion(){
     int choix;
     do {
-        std::cout << "\nPlease sign in :" << std::endl;
-        std::cout << "1. Governmental agency" << std::endl;
-        std::cout << "2. Providers" << std::endl;
-        std::cout << "3. Private individuals" << std::endl;
-        std::cout << "4. Test the app" << std::endl;
-        std::cout << "0. Leave the app" << std::endl;
-        std::cout << "Choice : ";
-        std::cin >> choix;
+        cout << "\nPlease sign in :" << endl;
+        cout << "1. Governmental agency" << endl;
+        cout << "2. Providers" << endl;
+        cout << "3. Private individuals" << endl;
+        cout << "4. Test the app" << endl;
+        cout << "0. Leave the app" << endl;
+        cout << "Choice : ";
+        cin >> choix;
 
         switch (choix) {
             case 1: menuAgent(); break;
             case 2: menuFournisseur(); break;
             case 3: menuUtilisateur(); break;
             case 4: test(); break;
-            case 0: std::cout << "Goodbye !" << std::endl; break;
-            default: std::cout << "Invalid choice. Please choose again :.\n";
+            case 0: cout << "Goodbye !" << endl; break;
+            default: cout << "Invalid choice. Please choose again :.\n";
         }
     } while (choix != 0);
 }
@@ -184,7 +184,7 @@ void afficherMenuConnexion(){
 
 
 int main() {
-    std::cout << "Welcome to AirWatcher !\n";
+    cout << "Welcome to AirWatcher !\n";
     afficherMenuConnexion();
     return 0;
 }
