8b63263178086c7c6690f2a537f121b842f337fa
lea.antonietti <lea.antonietti@insa-lyon.fr>
2025-05-14 15:51:05 +0200

    maj attribut

--- Attribut.cpp
+++ Attribut.cpp
@@ -7,3 +7,43 @@ using namespace std;
 #include  <array>
 
 #include "Attribut.h"
+
+
+//Constructeur
+Attribut::Attribut(int id, const string& unit, const string& description){
+    this->id = id;
+    this->unit = unit;
+    this->description = description;
+}
+
+Attribut::Attribut(const Attribut& attribut) {
+    this->unit = user.unit;
+    this->description = user.description;
+    this->id = user.id;
+}
+
+
+//Setters et getters
+void Attribut::setUnit(const string& unit) {
+    this->unit = unit;
+}
+
+string Attribut::getUnit() const {
+    return this->password;
+}
+
+void Attribut::setDescription(const string& description) {
+    this->description = description;
+}
+
+string Attribut::getDescription() const {
+    return this->description;
+}
+
+void Attribut::setId(int id) {
+    this->id = id;
+}
+
+int Attribut::getId() const {
+    return this->id;
+}
--- Attribut.h
+++ Attribut.h
@@ -6,3 +6,24 @@ using namespace std;
 #include <algorithm>
 #include  <array>
 
+class Attribut {
+protected:
+    //Atributes
+    int id;
+    string unit;
+    string description;
+
+    //Getters and Setters
+    void setId(int id);
+    void setUnit(const string& unit);
+    void setDescription(const string& description);
+    int getId() const;
+    string getUnit() const;
+    string getDescription() const;
+
+    //Constructors and Destructor
+    Attribut(int id, const string& unit, const string& description);
+    Attribut(const User& attribut);
+    ~Attribut();
+
+};
