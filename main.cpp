c7b5bf35a8b540e719a45b1b3ec30a791696bc51
lorisveron <loris.veron@insa-lyon.fr>
2025-05-14 15:07:10 +0200

    test

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
     e-mail               : $EMAIL$
 *************************************************************************/
 
-//---------- Réalisation de la classe <User> (fichier User.cpp) ------------
+//---------- Réalisation de la classe <Main> (fichier User.cpp) ------------
 
 //---------------------------------------------------------------- INCLUDE
 
@@ -58,6 +58,57 @@ void menuAgent(){
         std::cout << "4. Consult the impact of air cleaners" << std::endl;
         std::cout << "5. Analyse the data of a private individual" << std::endl;
         std::cout << "6. Consult the list of the excluded users" << std::endl;
+        std::cout << "0. Go back" << std::endl;
+        std::cout << "Choice : ";
+        std::cin >> choix;
+
+        switch (choix) {
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+            case 5: break;
+            case 6: break;
+            case 0: return;
+            default: std::cout << "Invalid choice. Please choose again :.\n";
+        }
+    } while (choix != 0);
+}
+
+void menuFournisseur(){
+    int choix;
+    do {
+        std::cout << "\nDear Provider, choose your action :" << std::endl;
+        std::cout << "1. Consult the quality of the air" << std::endl;
+        std::cout << "2. Compare a sensor to the other ones" << std::endl;
+        std::cout << "3. Consult statistics" << std::endl;
+        std::cout << "4. Consult the impact of air cleaners" << std::endl;
+        std::cout << "0. Go back" << std::endl;
+        std::cout << "Choice : ";
+        std::cin >> choix;
+
+        switch (choix) {
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+            case 0: return;
+            default: std::cout << "Invalid choice. Please choose again :.\n";
+        }
+    } while (choix != 0);
+    return;
+}
+
+void menuUtilisateur(){
+    int choix;
+    do {
+        std::cout << "\nDear User, choose your action :" << std::endl;
+        std::cout << "1. Consult the quality of the air" << std::endl;
+        std::cout << "2. Compare a sensor to the other ones" << std::endl;
+        std::cout << "3. Consult statistics" << std::endl;
+        std::cout << "4. Contribute data" << std::endl;
+        std::cout << "5. Consult my points" << std::endl;
+        std::cout << "6. Consult the data of my own sensors" << std::endl;
         std::cout << "0. Go back" << std::endl;
         std::cout << "Choice : ";
         std::cin >> choix;
