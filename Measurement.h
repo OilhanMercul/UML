42a2ef16bb3ff3df11fefc5cb6d173efe0715045
lea.antonietti <lea.antonietti@insa-lyon.fr>
2025-05-14 16:14:43 +0200

    maj measurement

--- Measurement.h
+++ Measurement.h
@@ -5,4 +5,37 @@ using namespace std;
 #include <string>
 #include <algorithm>
 #include  <array>
+#include "aircleaner.h" // pour la struct Date
+#include "Sensor.h"
+#include "Attribut.h"
 
+class Measurement {
+protected:
+    //Atributes
+    int id;
+    Date date;
+    Sensor sensor;
+    Attribut attribut;
+    float value;
+
+    // Getters
+    int getId() const;
+    Date getDate() const;
+    const Sensor& getSensor() const;
+    const Attribut& getAttribut() const;
+    float getValue() const;
+
+    // Setters
+    void setId(int newId);
+    void setDate(const Date& newDate);
+    void setSensor(const Sensor& newSensor);
+    void setAttribut(const Attribut& newAttribut);
+    void setValue(float newValue);
+
+
+    //Constructors and Destructor
+    Measurement(int id, const Date& date, const Sensor& sensor, const Attribut& attribut, float value);
+    Measurement(const Measurement& measurement);
+    ~Measurement();
+
+};
